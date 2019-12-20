287e76e97fa2ba89581b09bf9d88f706fd32c770
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-20 17:44:03 +0000

    build based on d7a684c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = RoadRunner","category":"page"},{"location":"#","page":"Home","title":"Home","text":"loada","category":"page"},{"location":"#RoadRunner.loada","page":"Home","title":"RoadRunner.loada","text":"loada(antString::String)\n\nTake an antimony string and return a roadrunner instance\n\n\n\n\n\n","category":"function"}]
+}
