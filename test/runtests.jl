c413ff07639897266eb29f65872911a45012859f
Jin <jin.xu.2010@gmail.com>
2020-09-21 14:34:06 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,23 @@
 using SafeTestsets
 
 @safetestset "my f Tests" begin include("my_f_test.jl") end
+
+#@safetestset "antimony test" begin include("antimony_test_0.jl") end
+
+#@safetestset "antimony test" begin include("antimony_test_1.jl") end
+
+#@safetestset "antimony test" begin include("antimony_test_2.jl") end
+
+#@safetestset "antimony test" begin include("antimony_test_3.jl") end
+
+#@safetestset "antimony test" begin include("antimony_test_4.jl") end
+
+#@safetestset "antimony test" begin include("antimony_test_5.jl") end
+
+#@safetestset "antimony test" begin include("antimony_test_6.jl") end
+
+#@safetestset "antimony test" begin include("antimony_test_7.jl") end
+
+#@safetestset "antimony test" begin include("antimony_test_8.jl") end
+
+#@safetestset "sbml test" begin include("sbml_test.jl") end
