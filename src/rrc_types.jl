a1d9df2e3475e01785f13a04361e724c51b6759c
Jin <jin.xu.2010@gmail.com>
2020-09-21 14:33:32 -0700

    Create rrc_types.jl

---
+++ src/rrc_types.jl
@@ -0,0 +1,26 @@
+struct RRVector
+end
+
+struct RRStringArray
+end
+
+struct RRDoubleMatrix
+end
+
+struct RRComplex
+end
+
+struct RRComplexVector
+end
+
+struct RRComplexMatrix
+end
+
+struct RRCData
+end
+
+struct RRList
+end
+
+struct RRListItem
+end
