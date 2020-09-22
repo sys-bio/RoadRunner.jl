3fb83d5b0cc9fbc54ba27c078ab966f22e3bc58e
Jin <jin.xu.2010@gmail.com>
2020-09-22 13:42:42 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,21 @@
 using SafeTestsets
 
-#@safetestset "antimony test 0" begin include("antimony_test_0.jl") end
+@safetestset "antimony test 0" begin include("antimony_test_0.jl") end
 
-#@safetestset "antimony test 1" begin include("antimony_test_1.jl") end
+@safetestset "antimony test 1" begin include("antimony_test_1.jl") end
 
-#@safetestset "antimony test 2" begin include("antimony_test_2.jl") end
+@safetestset "antimony test 2" begin include("antimony_test_2.jl") end
 
-#@safetestset "antimony test 3" begin include("antimony_test_3.jl") end
+@safetestset "antimony test 3" begin include("antimony_test_3.jl") end
 
-#@safetestset "antimony test 4" begin include("antimony_test_4.jl") end
+@safetestset "antimony test 4" begin include("antimony_test_4.jl") end
 
-#@safetestset "antimony test 5" begin include("antimony_test_5.jl") end
+@safetestset "antimony test 5" begin include("antimony_test_5.jl") end
 
-#@safetestset "antimony test 6" begin include("antimony_test_6.jl") end
+@safetestset "antimony test 6" begin include("antimony_test_6.jl") end
 
-#@safetestset "antimony test 7" begin include("antimony_test_7.jl") end
+@safetestset "antimony test 7" begin include("antimony_test_7.jl") end
 
 @safetestset "antimony test 8" begin include("antimony_test_8.jl") end
 
-#@safetestset "sbml test" begin include("sbml_test.jl") end
+@safetestset "sbml test" begin include("sbml_test.jl") end
