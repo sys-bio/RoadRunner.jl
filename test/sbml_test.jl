dbd02901776a58dc09337d19b355447860d61e3c
Jin <jin.xu.2010@gmail.com>
2020-09-21 17:57:08 -0700

    Update sbml_test.jl

--- test/sbml_test.jl
+++ test/sbml_test.jl
@@ -16,13 +16,13 @@ RoadRunner.loadSBML(rr, sbmlStr)
     @test RoadRunner.getGlobalParameterIds(rr) == ["J0_VM1", "J0_Keq1", "J0_h", "J4_V4", "J4_KS4"]
 end
 
-#@testset "species" begin
-#    @test RoadRunner.getNumberOfFloatingSpecies(rr) == 4
-#    @test RoadRunner.getFloatingSpeciesIds(rr) == ["S1", "S3", "S4", "S2"]
-#    @test RoadRunner.getNumberOfBoundarySpecies(rr) == 2
-#    @test RoadRunner.getBoundarySpeciesIds(rr) == ["X0", "X1"]
-#end
+@testset "species" begin
+    @test RoadRunner.getNumberOfFloatingSpecies(rr) == 4
+    @test RoadRunner.getFloatingSpeciesIds(rr) == ["S1", "S2", "S3", "S4"]
+    @test RoadRunner.getNumberOfBoundarySpecies(rr) == 2
+    @test RoadRunner.getBoundarySpeciesIds(rr) == ["X0", "X1"]
+end
 
-#@testset "steadystates" begin
-#    @test RoadRunner.steadyState(rr) < 1e-6
-#end
+@testset "steadystates" begin
+    @test RoadRunner.steadyState(rr) < 1e-6
+end
