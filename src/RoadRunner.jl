6c381b776bb81fa2fa489a3c8d654bb2e7052994
Jin <jin.xu.2010@gmail.com>
2021-04-30 13:52:01 -0700

    Update RoadRunner.jl

--- src/RoadRunner.jl
+++ src/RoadRunner.jl
@@ -477,7 +477,7 @@ Retrieve the current error string. Example, str = getLastError (void);
 function getLastError()
   char_pointer = ccall(dlsym(rrlib, :getLastError), cdecl, Ptr{UInt8}, ())
   julia_str = unsafe_string(char_pointer)
-  freeText(char_pointer)
+  #freeText(char_pointer)
   return julia_str
 end
 ###############################################################################
