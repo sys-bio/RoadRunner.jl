eb40c8288329091e4cf2d092a5b6b6066757ccea
Adel Heydarabadipour <36909449+adelhpour@users.noreply.github.com>
2024-05-16 13:27:00 -0700

    Refactoring the syntax of the error_test.jl

--- test/error_test.jl
+++ test/error_test.jl
@@ -2,15 +2,20 @@ using RoadRunner
 using Test
 
 
-@testset "error test" begin
+@testset "test roadrunner errors" begin
+    ant_str = """
+        A -> B; k1*A,
+        A = 10,
+    """
+    @test_throws ErrorException("Error in model string, line 1:  syntax error, unexpected ',', expecting end of line or ';' or '\\n'") RoadRunner.loada(ant_str)
+end
 
-    # Test that antimony errors are using the getLastAntimonyError function
-    bad_antimony = "A -> B; k1*A, A=10,"
-    @test_throws ErrorException("Error in model string, line 1:  syntax error, unexpected ',', expecting end of line or ';' or '\\n'") RoadRunner.loada(bad_antimony)
-
-    # Test that roadrunner errors are using the getLastError function from the RoadRunner.jl module
-    test_antimony = "A -> B; k1*A; A=10; k1=5"
-    r = RoadRunner.loada(test_antimony)
-    @test_throws ErrorException("RoadRunner exception: No sbml element exists for symbol 'S'\n") RoadRunner.getValue(r, "S")
-    
+@testset "test antimony errors" begin
+    ant_str = """
+        A -> B; k1*A
+        A = 10
+        k1 = 5
+    """
+    rr = RoadRunner.loada(ant_str)
+    @test_throws ErrorException("RoadRunner exception: No sbml element exists for symbol 'S'\n") RoadRunner.getValue(rr, "S")
 end
